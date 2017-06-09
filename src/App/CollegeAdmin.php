9df2bd6030dd3e7227d290940c7bfff5bd823d85
Ayush <ayushpahwa96@gmail.com>
2017-06-09 19:35:21 +0530

    Reindents

--- src/App/CollegeAdmin.php
+++ src/App/CollegeAdmin.php
@@ -1,28 +1,28 @@
 <?php
 
- 
 
- namespace App;
+
+namespace App;
 
 use Spot\EntityInterface as Entity;
 use Spot\MapperInterface as Mapper;
- use Spot\EventEmitter;
- use App\Student;
+use Spot\EventEmitter;
+use App\Student;
 
 
- use Tuupola\Base62;
+use Tuupola\Base62;
 
- use Ramsey\Uuid\Uuid;
- use Psr\Log\LogLevel;
- 
- class CollegeAdmin extends \Spot\Entity
- {
+use Ramsey\Uuid\Uuid;
+use Psr\Log\LogLevel;
+
+class CollegeAdmin extends \Spot\Entity
+{
     protected static $table = "collegeadmins";
 
     public static function fields()
     {
         return [
-       
+        
 
         "id" => ["type" => "integer" , "unsigned" => true, "primary" => true, "autoincrement" => true],
         "username" => ["type" => "integer"],
@@ -35,18 +35,18 @@ use Spot\MapperInterface as Mapper;
     {
         $emitter->on("beforeInsert", function (EntityInterface $entity, MapperInterface $mapper) {
             $entity->id = Base62::encode(random_bytes(9));
-            });
+        });
     }
 
     public function clear()
     {
         $this->data([
-            ]);
+                    ]);
     }
     public static function relations(Mapper $mapper, Entity $entity)
     {
         return [
-            'CollegeAdmin' => $mapper->belongsTo($entity, 'App\College', 'college_id')
-            ];
+        'CollegeAdmin' => $mapper->belongsTo($entity, 'App\College', 'college_id')
+        ];
     }
 }
