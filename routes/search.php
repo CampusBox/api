0e824d561e0c78761037e4575f2b5ba48df99e60
Aditya Chawla <chawlaaditya8@gmail.com>
2017-06-12 02:01:46 +0530

    student and skill search working

--- routes/search.php
+++ routes/search.php
@@ -29,9 +29,39 @@ $app->get("/search/students/{query}", function ($request, $response, $arguments)
   }
 
   $students = $this->spot->mapper("App\Student")->query('
-                                                        SELECT * FROM students
-                                                        WHERE name LIKE "% '.$arguments['query'].'%" 
-                                                        OR name LIKE "'.$arguments['query'].'%"
+SELECT 
+students.name,
+students.username, 
+student_skills.skill_name, 
+(CASE 
+  WHEN (student_skills.skill_name LIKE "'.$arguments['query'].'%") 
+  THEN 10 ELSE 0 END) AS score3, 
+(CASE 
+  WHEN (student_skills.skill_name LIKE "%'.$arguments['query'].'%") 
+  THEN 10 ELSE 0 END) AS score3, 
+(CASE 
+  WHEN (name LIKE "%'.$arguments['query'].'%") 
+  THEN 50 ELSE 0 END) AS score2, 
+(CASE 
+  WHEN (name LIKE "'.$arguments['query'].'%") 
+  THEN 50 ELSE 0 END) AS score2, 
+(CASE 
+  WHEN (students.username LIKE "%'.$arguments['query'].'%") 
+  THEN 150 ELSE 0 END) AS score1, 
+(CASE 
+  WHEN (students.username LIKE "'.$arguments['query'].'%") 
+  THEN 150 ELSE 0 END) AS score1 
+FROM students 
+LEFT JOIN student_skills 
+ON students.username = student_skills.username 
+WHERE student_skills.skill_name LIKE "'.$arguments['query'].'%"
+OR student_skills.skill_name LIKE "%'.$arguments['query'].'%"
+OR name LIKE "%'.$arguments['query'].'%"
+OR name LIKE "'.$arguments['query'].'%"
+OR students.username LIKE "%'.$arguments['query'].'%"
+OR students.username LIKE "'.$arguments['query'].'%"
+GROUP BY students.username 
+ORDER BY score1 DESC,score2 DESC,score3 DESC
                                                         ');
 
   if(isset($students) ){
@@ -112,9 +142,39 @@ $app->get("/search/{query}", function ($request, $response, $arguments) {
   $query =isset($arguments["query"])?isset($arguments["query"]):" ";
 
   $students = $this->spot->mapper("App\Student")->query('
-                                                        SELECT * FROM students
-                                                        WHERE name LIKE "% '.$arguments['query'].'%" 
-                                                        OR name LIKE "'.$arguments['query'].'%"
+SELECT 
+students.name,
+students.username, 
+student_skills.skill_name, 
+(CASE 
+  WHEN (student_skills.skill_name LIKE "'.$arguments['query'].'%") 
+  THEN 10 ELSE 0 END) AS score3, 
+(CASE 
+  WHEN (student_skills.skill_name LIKE "%'.$arguments['query'].'%") 
+  THEN 10 ELSE 0 END) AS score3, 
+(CASE 
+  WHEN (name LIKE "%'.$arguments['query'].'%") 
+  THEN 50 ELSE 0 END) AS score2, 
+(CASE 
+  WHEN (name LIKE "'.$arguments['query'].'%") 
+  THEN 50 ELSE 0 END) AS score2, 
+(CASE 
+  WHEN (students.username LIKE "%'.$arguments['query'].'%") 
+  THEN 150 ELSE 0 END) AS score1, 
+(CASE 
+  WHEN (students.username LIKE "'.$arguments['query'].'%") 
+  THEN 150 ELSE 0 END) AS score1 
+FROM students 
+LEFT JOIN student_skills 
+ON students.username = student_skills.username 
+WHERE student_skills.skill_name LIKE "'.$arguments['query'].'%"
+OR student_skills.skill_name LIKE "%'.$arguments['query'].'%"
+OR name LIKE "%'.$arguments['query'].'%"
+OR name LIKE "'.$arguments['query'].'%"
+OR students.username LIKE "%'.$arguments['query'].'%"
+OR students.username LIKE "'.$arguments['query'].'%"
+GROUP BY students.username 
+ORDER BY score1 DESC,score2 DESC,score3 DESC
                                                         ');
   $resourceStudents = new Collection($students, new StudentMiniTransformer);
 
@@ -154,66 +214,6 @@ $app->get("/search/{query}", function ($request, $response, $arguments) {
     $arr = $fractal->createData($resourceEvents)->toArray();
     $data['event'] = $arr;
   }
-
-// $events = $this->spot->mapper("App\Event")
-  //   ->query("SELECT *, MATCH (title) AGAINST ".
-  //       "('".$query."*' IN BOOLEAN MODE) AS score1,". 
-  //       "MATCH (subtitle) AGAINST ('".$query."*' IN BOOLEAN MODE) AS score2,".
-  //       "MATCH (description) AGAINST ('".$query."*' IN BOOLEAN MODE) AS score3 ".
-  //       "FROM events".
-  //       " WHERE MATCH(title) ".
-  //       "AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) ".
-  //       "or  MATCH(subtitle) AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)".
-  //       "or MATCH(description) AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)".
-  //       "or MATCH(title) AGAINST('".$query."*' IN BOOLEAN MODE) ".
-  //       "OR MATCH(subtitle) AGAINST('".$query."*' IN BOOLEAN MODE) ".
-  //           "OR title LIKE '%".$query."%'  
-  //           OR subtitle LIKE '%".$query."%'  ".
-  //           "OR description LIKE '%".$query."%'  ".
-  //           "order by score1 desc,score2 desc, score3 desc,events.to_date desc limit 3" );
-  // 
-  // $content = $this->spot->mapper("App\Content")
-  //   ->query("SELECT *, 
-  //       CASE WHEN followers.followed_username IS NULL THEN 0 ELSE 8 END AS score,
-  //       CASE WHEN content_appreciates.content_id IS NULL THEN 0 ELSE 1 END AS score1
-  //       FROM contents
-  //       LEFT JOIN followers
-  //       ON contents.created_by_username = followers.followed_username
-  //       LEFT JOIN content_appreciates
-  //       ON contents.content_id = content_appreciates.content_id
-  //       GROUP BY contents.content_id
-  //       ORDER BY score1 DESC ,contents.timer desc limit 2");
-  // 
-  //   $content2 = $this->spot->mapper("App\Content")
-  //   ->query("SELECT *, MATCH (title) AGAINST ".
-  //       "('".$query."*' IN BOOLEAN MODE) AS score1 ". 
-  //       "FROM contents WHERE MATCH(title) ".
-  //       "AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) ".
-  //       "or MATCH(title) AGAINST('".$query."*' IN BOOLEAN MODE) ".
-  //       "OR title LIKE '%".$query."%'  ".
-  //       "order by score1 desc,  contents.timer desc limit 3" );
-  // 
-  // $students = $this->spot->mapper("App\Student")
-    // ->query("SELECT *, MATCH (name) AGAINST ('".$query."*' IN BOOLEAN MODE) AS score1,".
-    //  "MATCH (username) AGAINST ('".$query."*' IN BOOLEAN MODE) AS score2,".
-    //  "MATCH (about) AGAINST ('".$query."*' IN BOOLEAN MODE) AS score3 ".
-    //  "FROM students WHERE MATCH(name) AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) ".
-    //  "or  MATCH(username) AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) ".
-    //  "or MATCH(about) AGAINST('".$query."*' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) ".
-    //  "or MATCH(name) AGAINST('".$query."*' IN BOOLEAN MODE) ".
-    //  "OR MATCH(username) AGAINST('".$query."*' IN BOOLEAN MODE) ".
-    //  "OR name LIKE '%".$query."%'  ".
-    //  "OR username LIKE '%".$query."%'  ".
-    //  "OR about LIKE '%".$query."%'  ".
-    //  "order by score1 desc,score2 desc, score3 desc limit 2" );
-
-
-    // $events = $this->spot->mapper("App\Event")
-    // ->query(("SELECT * from events where title LIKE '%".$query."%' limit 2" ));
-  // $students = $this->spot->mapper("App\Student")
-  //   ->query(("SELECT * from students where name LIKE '%".$query."%' limit 3" ));
-  //   $content = $this->spot->mapper("App\Content")
-//   ->query(("SELECT * from contents where title LIKE '%".$query."%' limit 3" ));
 
     return $response->withStatus(200)
     ->withHeader("Content-Type", "application/json")
