a485d3f8bc3c3a5bec69f88bdbdffe6cef8b1215
Ayush <ayushpahwa96@gmail.com>
2017-06-18 23:01:51 +0530

    Changed order of responses and sending added object in post  req

--- routes/content/contentActions.php
+++ routes/content/contentActions.php
@@ -19,44 +19,33 @@ use Firebase\JWT\JWT;
 use Tuupola\Base62;
 
 
-
+/**
+ * Gets res
+ */
 $app->get("/responses/{content_id}", function ($request, $response, $arguments) {
 
   $limit = isset($_GET['limit']) ? $_GET['limit'] : 3;
   $offset = isset($_GET['offset']) ? $_GET['offset'] : 0;
 
-  $token = $request->getHeader('authorization');
-  $token = substr($token[0], strpos($token[0], " ") + 1); 
-  $JWT = $this->get('JwtAuthentication');
-  $token = $JWT->decodeToken($JWT->fetchToken($request));
-
-  if ($token) 
-    $test = $token->username;
-  else
-    $test = '0';
-
   if(isset($arguments['content_id'])){
     $responses = $this->spot->mapper("App\ContentResponses")
     ->where(["content_id"=>$arguments['content_id'], "status"=>0])
-    ->limit($limit, $offset);
-  }else{
-
-    $responses = $this->spot->mapper("App\ContentResponses")
-    ->where(["status"=>0])
-    ->limit($limit, $offset);
+    ->limit($limit, $offset)
+    ->order(["content_response_id" => "DESC"]);
   }
+
   $offset += $limit;
 
   /* Serialize the response data. */
   $fractal = new Manager();
   $fractal->setSerializer(new DataArraySerializer);
 
-  if (isset($_GET['include'])) {
-    $fractal->parseIncludes($_GET['include']);
+  if ($responses && sizeof($responses) > 0) {
+    $resource = new Collection($responses, new ContentResponsesTransformer());
+    $data = $fractal->createData($resource)->toArray();
+  } else{
+    $data['data'] = [];
   }
-
-  $resource = new Collection($responses, new ContentResponsesTransformer(['username' => $test, 'type' => 'get']));
-  $data = $fractal->createData($resource)->toArray();
 
   $data['meta']['offset'] = $offset;
   $data['meta']['limit'] = $limit;
@@ -79,20 +68,45 @@ $app->post("/contentResponse/{content_id}", function ($request, $response, $argu
    $contentresponse['status'] = 0;
 
    $newresponse = new ContentResponses($contentresponse);
-   $this->spot->mapper("App\ContentResponses")->save($newresponse);
-   $data["status"] = "ok";
-   $data["message"] = "Response added";
-   return $response->withStatus(201)
-   ->withHeader("Content-Type", "application/json")
-   ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-   }
-   else{
-   $data["status"] = "error";
-   $data["message"] = "No response text found";
-   return $response->withStatus(406)
-   ->withHeader("Content-Type", "application/json")
-   ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-   }
+   $mapper = $this->spot->mapper("App\ContentResponses");
+   $id = $mapper->save($newresponse);
+
+   if ($id) {
+
+     /* Serialize the response data. */
+     $fractal = new Manager();
+     $fractal->setSerializer(new DataArraySerializer);
+
+     $entity = $mapper->where(["content_response_id"=>$id, "status"=>0]);
+
+     $data["status"] = "ok";
+     $data["message"] = "Response added";
+     
+     $resource = new Collection($entity, new ContentResponsesTransformer());
+     $data["response"] = $fractal->createData($resource)->toArray()['data'][0];
+
+     return $response->withStatus(201)
+     ->withHeader("Content-Type", "application/json")
+     ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+   } else {
+
+    $data["status"] = "error";
+    $data["message"] = "Error in inserting!";
+
+    return $response->withStatus(500)
+    ->withHeader("Content-Type", "application/json")
+    ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+  }
+
+
+}
+else{
+ $data["status"] = "error";
+ $data["message"] = "No response text found";
+ return $response->withStatus(406)
+ ->withHeader("Content-Type", "application/json")
+ ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+}
 });
 
 
@@ -107,9 +121,9 @@ $app->delete("/contentResponse/{content_response_id}", function ($request, $resp
     throw new ForbiddenException("Token not found", 404);
   }
   if (false === $contentresponse = $this->spot->mapper("App\ContentResponses")->first([
-    "content_response_id" => $arguments["content_response_id"],
-    "username" =>  $this->token->decoded->username, "status" => 0
-    ])) {
+                                                                                      "content_response_id" => $arguments["content_response_id"],
+                                                                                      "username" =>  $this->token->decoded->username, "status" => 0
+                                                                                      ])) {
     throw new NotFoundException("Response wasn't there.", 404);
 }
 
@@ -117,25 +131,25 @@ if ( $contentresponse->username != $token->username)  {
   throw new ForbiddenException("Only the owner can delete the response", 404);
 }
 
-  $update_response = $this->spot->mapper("App\ContentResponses")->first(["content_response_id" => $arguments["content_response_id"]]);
+$update_response = $this->spot->mapper("App\ContentResponses")->first(["content_response_id" => $arguments["content_response_id"]]);
 
-  if ($update_response) {
-    $update_response->status = 1;
-    $status = $this->spot->mapper("App\ContentResponses")->update($update_response);
-  }
+if ($update_response) {
+  $update_response->status = 1;
+  $status = $this->spot->mapper("App\ContentResponses")->update($update_response);
+}
 
-  $update_response = $this->spot->mapper("App\ContentResponses")->first(["content_response_id" => $arguments["content_response_id"]]);
+$update_response = $this->spot->mapper("App\ContentResponses")->first(["content_response_id" => $arguments["content_response_id"]]);
 
-  if ($update_response) {
-    $update_response->status = 1;
-    $status = $this->spot->mapper("App\ContentResponses")->update($update_response);
-  }
+if ($update_response) {
+  $update_response->status = 1;
+  $status = $this->spot->mapper("App\ContentResponses")->update($update_response);
+}
 
-  $data["status"] = $status;
-  $data["message"] = "Response updated.";
-  return $response->withStatus(201)
-  ->withHeader("Content-Type", "application/json")
-  ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+$data["status"] = $status;
+$data["message"] = "Response updated.";
+return $response->withStatus(201)
+->withHeader("Content-Type", "application/json")
+->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 });
 
 
@@ -147,28 +161,28 @@ $app->patch("/contentResponse/{content_response_id}", function ($request, $respo
   $token = $JWT->decodeToken($JWT->fetchToken($request));
 
   if (false === $contentresponse = $this->spot->mapper("App\ContentResponses")->first([
-    "content_response_id" => $arguments["content_response_id"],
-    "username" =>  $this->token->decoded->username, "status" => 0
-    ])) {
+                                                                                      "content_response_id" => $arguments["content_response_id"],
+                                                                                      "username" =>  $this->token->decoded->username, "status" => 0
+                                                                                      ])) {
     throw new NotFoundException("Response wasn't there.", 404);
-  }
+}
 
-  $body = $request->getParsedBody();
+$body = $request->getParsedBody();
 
-  $update_response = $this->spot->mapper("App\ContentResponses")->first(["content_response_id" => $arguments["content_response_id"]]);
+$update_response = $this->spot->mapper("App\ContentResponses")->first(["content_response_id" => $arguments["content_response_id"]]);
 
-  if ($update_response) {
-    $data["orig"] = $update_response->response_text;
-    $update_response->response_text = $body['response_text'];
-    $data["new"] = $update_response->response_text;
-    $status = $this->spot->mapper("App\ContentResponses")->update($update_response);
-  }
+if ($update_response) {
+  $data["orig"] = $update_response->response_text;
+  $update_response->response_text = $body['response_text'];
+  $data["new"] = $update_response->response_text;
+  $status = $this->spot->mapper("App\ContentResponses")->update($update_response);
+}
 
-  $data["status"] = $status;
-  $data["message"] = "Response updated.";
-  return $response->withStatus(201)
-  ->withHeader("Content-Type", "application/json")
-  ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+$data["status"] = $status;
+$data["message"] = "Response updated.";
+return $response->withStatus(201)
+->withHeader("Content-Type", "application/json")
+->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 });
 
 
@@ -182,9 +196,9 @@ $app->post("/bookmarkContent/{content_id}", function ($request, $response, $argu
  $bookmark = new ContentBookmarks($body);
 
  if (false === $check = $this->spot->mapper("App\ContentBookmarks")->first([
-  "content_id" => $arguments["content_id"],
-  "username" =>  $this->token->decoded->username
-  ])) {
+                                                                           "content_id" => $arguments["content_id"],
+                                                                           "username" =>  $this->token->decoded->username
+                                                                           ])) {
   $this->spot->mapper("App\ContentBookmarks")->save($bookmark);
 
 }else {
@@ -205,9 +219,9 @@ $app->delete("/bookmarkContent/{content_id}", function ($request, $response, $ar
 
   /* Load existing bookmark using provided content_id */
   if (false === $bookmark = $this->spot->mapper("App\ContentBookmarks")->first([
-    "content_id" => $arguments["content_id"],
-    "username" =>  $this->token->decoded->username
-    ])) {
+                                                                               "content_id" => $arguments["content_id"],
+                                                                               "username" =>  $this->token->decoded->username
+                                                                               ])) {
     throw new NotFoundException("Had never bookmarked it.", 404);
 }
 $this->spot->mapper("App\ContentBookmarks")->delete($bookmark);
@@ -229,9 +243,9 @@ $app->post("/appreciateContent/{content_id}", function ($request, $response, $ar
  $appreciate = new ContentAppreciate($body);
 
  if (false === $check = $this->spot->mapper("App\ContentAppreciate")->first([
-  "content_id" => $arguments["content_id"],
-  "username" =>  $this->token->decoded->username
-  ])) {
+                                                                            "content_id" => $arguments["content_id"],
+                                                                            "username" =>  $this->token->decoded->username
+                                                                            ])) {
 
   $this->spot->mapper("App\ContentAppreciate")->save($appreciate);
 } else  {
@@ -249,9 +263,9 @@ $app->delete("/appreciateContent/{content_id}", function ($request, $response, $
 
  /* Load existing appreciate using provided content_id */
  if (false === $appreciate = $this->spot->mapper("App\ContentAppreciate")->first([
-  "content_id" => $arguments["content_id"],
-  "username" =>  $this->token->decoded->username
-  ])) {
+                                                                                 "content_id" => $arguments["content_id"],
+                                                                                 "username" =>  $this->token->decoded->username
+                                                                                 ])) {
   throw new NotFoundException("Had never appreciateed it.", 404);
 };
 
--- src/App/Content/ContentResponsesTransformer.php
+++ src/App/Content/ContentResponsesTransformer.php
@@ -1,6 +1,6 @@
 <?php
 
- 
+
 
 namespace App;
 
@@ -11,16 +11,13 @@ class ContentResponsesTransformer extends Fractal\TransformerAbstract {
 
 	public function transform(ContentResponses $contentresponses) {
 		return [
-			"content_response_id" => (integer) $contentresponses->content_response_id ?: 0,
-			"content_id" => (integer) $contentresponses->content_id ?: 0,
-			"username" => (string) $contentresponses->username ?: null,
-			"response_text" => (string) $contentresponses->response_text ?: null,
-
-			/**
-			 * This part has to be replaced with data from studentMiniTransformer
-			 */
-			"name" => "Ayush Pahwa",
-			"image" => "https://scontent.xx.fbcdn.net/v/t1.0-1/13051612_1088563414516302_8913007315739796978_n.jpg?oh=588ab4000434e319175c17aab94ea0c1&oe=5950465C",
+		"content_response_id" => (integer) $contentresponses->content_response_id ?: 0,
+		"content_id" => (integer) $contentresponses->content_id ?: 0,
+		"response_text" => (string) $contentresponses->response_text ?: null,
+		"username" => (string) $contentresponses->Student['username'] ?: null,
+		"name" => (string) $contentresponses->Student['name'] ?: null,
+		"about" => (string) $contentresponses->Student['about'] ?: null,
+		"photo" => (string) $contentresponses->Student['image'] ?: null,
 		];
 	}
 }
